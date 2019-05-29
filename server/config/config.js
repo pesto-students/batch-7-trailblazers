b99050447ef51748bd937ea539eb4bc54d68f647
manish zanzad <manishzanzad@gmail.com>
2019-05-30 04:49:48 +0530

    Deploy mongo db on Atlas and Added DB configuration in config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,11 @@
-const {PORT} = process.env;
+const { PORT, DATABASE_CONNECTION_STRING } = process.env;
 
 export default {
-    server : {
-        SERVER_PORT : PORT || 8000, 
-    }
+  server: {
+    SERVER_PORT: PORT || 8000
+  },
+  database: {
+    CONNECTION_STRING:
+      DATABASE_CONNECTION_STRING || "mongodb://localhost:27017/issues-tracker"
+  }
 };
