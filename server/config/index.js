515f0298cbc872118aab7b9d5381e61b688eee74
manish zanzad <manishzanzad@gmail.com>
2019-05-31 22:00:37 +0530

    test: testing the db connection on travis

--- server/config/index.js
+++ server/config/index.js
@@ -5,6 +5,8 @@ export default {
     PORT: PORT || 8000,
   },
   database: {
-    CONNECTION_URL: DATABASE_CONNECTION_STRING || 'mongodb://localhost:27017/issues-tracker',
+    CONNECTION_URL:
+      'mongodb+srv://trailblazer:trailblazer@issues-tracker-juky2.mongodb.net/test?retryWrites=true&w=majority'
+      || 'mongodb://localhost:27017/issues-tracker',
   },
 };
