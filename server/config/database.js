f207464f59ee074c15a166c0c313b9262f746a74
manish zanzad <manishzanzad@gmail.com>
2019-06-01 03:50:00 +0530

    Added Console.log

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose';
 import config from './index';
-import server from '../app';
 
 export default {
   async connectDB() {
     try {
       const { CONNECTION_URL } = config.database;
       await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true });
-      server.close();
+      console.log('in Connect DB: DB connection successfull');
     } catch (exception) {
       console.log(`Exception occurred while connecting to Mongo ${exception}`);
       // process.exit(1);
