dd5022577b61389d6d2148acdc8f2ea1545553c0
VikalpP <vikalppanseriya@gmail.com>
2019-06-11 07:33:32 +0530

    feat: add middleware to authenticate user to access api

---
+++ server/middlewares/authenticateRoute.js
@@ -0,0 +1,9 @@
+import { buildResponse } from '../utils/helpers';
+
+export default (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+
+  const errorMessage = 'User is not authenticated!';
+  const response = buildResponse(false, errorMessage);
+  return res.status(403).send(response);
+};
