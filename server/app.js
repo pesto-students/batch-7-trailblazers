2b73016e65eb8efe4270dbb07c60dd7162210b88
manish zanzad <manishzanzad@gmail.com>
2019-06-18 03:23:34 +0530

    Added cors to enable options method

--- server/app.js
+++ server/app.js
@@ -36,10 +36,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', 'https://issuestracker-tb.netlify.com');
   res.set('Access-Control-Allow-Credentials', 'true');
   return next();
 });
+app.options('*', cors());
 require('./config/passport');
 
 app.use('/', router);
